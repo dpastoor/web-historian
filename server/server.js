2c29f0d0f3608edfc613e82d34857648633c98b5
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-16 20:16:22 -0800

    add morgan for logging and bodyparser to send json cleanly to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 let express = require('express');
 let path = require('path');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
 let app = express();
 
 
+app.use(morgan('dev'));
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
    console.log(req)
