4ac713173bff264f54e20c896273730c0d118721
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-16 20:11:36 -0800

    convert to send all files from the client folder

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 let express = require('express');
 let path = require('path');
 let app = express();
+
+
+app.use(express.static('client'));
+
 app.get('/', (req, res) =>
-  // res.sendfile takes an absolute path to a file and
-  // sets the mime type based on the file extension
-  // to resolve urls can also use the path module to manage proper '/' resolution
-  res.sendFile(path.resolve(__dirname, '../client/index.html'), (err) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-  })
-  //don't need the error handling could also do
-  // res.sendfile(__dirname = '/index.html')
+   console.log(req)
 );
 
 
