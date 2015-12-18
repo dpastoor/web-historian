25580ab716e87cc7957b0719cb1b769cab136d26
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-17 21:51:29 -0800

    use redis for key value store

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ let path = require('path');
 let bodyParser = require('body-parser');
 let morgan = require('morgan');
 let cors = require('cors');
+let redis = require('redis');
+let _ = require('lodash');
 let app = express();
 let sites = {'google': {
                     queued: false,
@@ -18,21 +20,35 @@ let sites = {'google': {
                     queued: false,
                     site: 'www.costco.com',
                     html: 'other text'
+             },
+             'facebook' : {
+                    queued: false,
+                    site: 'www.facebook.com',
+                    html: 'more text'
               }
 };
 
+let client = redis.createClient();
+_.forEach(sites, function(s, key) {
+  client.set(key, JSON.stringify(s));
+});
+
+client.get('google', (err, value) => console.log(value));
+client.get('facebook', (err, value) => console.log(value));
 app.use(morgan('dev'));
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 app.param('sites', (req, res, next, site) => {
-  if (sites[site]) {
-    req.site = sites[site];
-    next();
-  } else {
-    res.status(404).sendFile(path.resolve(__dirname, "../client/404notfound.html"));
-  }
+  client.get(site, (err, value) => {
+    if (value) {
+      req.site = JSON.parse(value);
+      next();
+    } else {
+      res.status(404).sendFile(path.resolve(__dirname, "../client/404notfound.html"));
+    }
+  });
 });
 
 app.get('/:sites', (req, res) => {
